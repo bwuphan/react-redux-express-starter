d202f8511ce676a003e2d6bdbdb96be125aada58
Bobby <bwuphan@gmail.com>
2017-09-07 20:07:13 -0700

    Configure Root.js

--- client/src/Root.js
+++ client/src/Root.js
@@ -4,6 +4,7 @@ import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import configureStore from './configureStore';
 import App from './App';
+import Signup from './components/signup/Signup';
 
 const store = configureStore();
 const history = syncHistoryWithStore(browserHistory, store);
@@ -12,7 +13,8 @@ const Root = () =>
   <Provider store={store}>
     <Router history={history}>
       <Route path="/" component={App}>
-        <IndexRoute component={App} />
+        <IndexRoute component={Signup} />
+        <Route path="/signup" component={Signup} />
       </Route>
     </Router>
   </Provider>;
